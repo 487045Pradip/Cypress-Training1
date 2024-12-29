c57a99cd84bed3563c763dec939213c08d103611
487045Pradip <487045@cognizant.com>
2024-12-29 09:49:15 +0530

    commit on 29 Dec

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) {      
       // implement node event listeners here
       config.specPattern = [
         'cypress/e2e/**/*.spec.js','cypress/e2e/test-automation/tests/*.feature'
--- cypress/e2e/TC2-Login-Positive_Negetive.spec.js
+++ cypress/e2e/TC2-Login-Positive_Negetive.spec.js
@@ -19,14 +19,20 @@ describe('Login Scenario', ()=>{
         login.clickLoginSignup()
         login.typeEmail(userData.email)
         login.typePassword(`${userData.password} wrong`)
+        //cy.pause()
+        cy.get('[data-qa="login-button"]').screenshot("Manual- Login Button Screenshot")
         login.clickLoginButton()
         login.verifyLoginFaliedForInvalidCr()
         //login.verifyLoggedIn(userData.username)
         //login.ClickLogout()
+        // For failed screenshots
+        //cy.get('.login-form > form > p').should('have.text', 'Your emai or password is incorrect!');
+        
         
     })
 
     it('TC_Login_Positive_using_CustomCommand', ()=>{ 
         cy.login(userData.email,userData.username, userData.password);
-    })
+        cy.screenshot("Manual- Login Successful");
+     })
 })
